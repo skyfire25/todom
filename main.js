d2c9fd9e0ae7fbb6558a420559dbce289b6a47c7
Carlos L Molina <carlos.molina@codeimmersives.com>
2020-03-23 11:05:47 -0400

    changes

---
+++ main.js
@@ -0,0 +1,17 @@
+let addTodoButton = document.querySelector('.add-todo');
+let inputValue = document.querySelector('.todo-input');
+
+function printTodo() {
+console.log(inputValue.value)
+}
+
+addTodoButton.addEventListener('click', printTodo);
+
+function removeFromArray(){
+
+}
+
+function refreshList(){
+
+
+}
